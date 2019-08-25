aa145586020450d4bd6457c9a5ba734281682b30
Shai Gerbi <ShaiGerbi@gmail.com>
2019-08-25 21:02:27 +0300

    Default config

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import '../css/main.css';
+
+let arrowFunction = () => {
+    console.log('babel');
+};
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports =
+{
+    mode: 'production', // development or production
+    entry: './src/js/app.js',
+
+    output:
+    {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+    },
+
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.css$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            publicPath: 'images/',
+                        },
+                    },
+                ],
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+				{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: false,
+                    },
+                }],
+            },
+        ],
+    },
+
+    plugins:
+    [
+        new HtmlWebpackPlugin(
+        {
+            template: 'src/index.html',
+            minify: {
+                removeComments: true,
+                removeStyleTypeAttributes: true,
+                removeScriptTypeAttributes: true,
+                minifyCSS: true,
+                minifyJS: true,
+                collapseWhitespace: true,
+                keepClosingSlash: true,
+            },
+        }),
+    ]
+};
